1e713c8f9dd44f596c182cc802ab30bdbfdfb421
LCL121 <1391436522@qq.com>
2021-06-11 20:14:49 +0800

    step 6 end

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 /** @jsxRuntime classic */
-function createTextElement(text) {
+function createTextElement (text) {
   return {
     type: 'TEXT_ELEMENT',
     props: {
@@ -9,12 +9,12 @@ function createTextElement(text) {
   }
 }
 
-function createElement(type, props, ...children) {
+function createElement (type, props, ...children) {
   return {
     type,
     props: {
       ...props,
-      children: children.map(child => 
+      children: children.map(child =>
         typeof child === 'object'
           ? child
           : createTextElement(child)
@@ -23,52 +23,129 @@ function createElement(type, props, ...children) {
   }
 }
 
-function createDom(fiber) {
+function createDom (fiber) {
   const dom = fiber.type === 'TEXT_ELEMENT'
     ? document.createTextNode('')
     : document.createElement(fiber.type)
-  
-  const isProperty = key => key !== 'children'
-  Object.keys(fiber.props)
-    .filter(isProperty)
-    .forEach(name => {
-      dom[name] = fiber.props[name]
-    })
+
+  updateDom(dom, {}, fiber.props)
 
   return dom
 }
 
-function commitRoot() {
+/**
+ * update Dom
+ */
+const isEvent = key => key.startsWith('on')
+// 过滤出不是children 并且不是event 的属性
+const isProperty = key => key !== "children" && !isEvent(key)
+// 过滤出新属性
+const isNew = (prev, next) => key => prev[key] !== next[key]
+// 过滤出不在next 的属性（不需要的old 属性）
+const isGone = (prev, next) => key => !(key in next)
+function updateDom (dom, prevProps, nextProps) {
+  // Remove old or changed event listeners
+  Object.keys(prevProps)
+    .filter(isEvent)
+    // 过滤出remove 或者changed 的event
+    .filter(
+      key => 
+        !(key in nextProps) || 
+        isNew(prevProps, nextProps)(key)
+    )
+    .forEach(name => {
+      const eventType = name.toLowerCase().substring(2)
+      dom.removeEventListener(eventType, prevProps[name])
+    })
+  // Remove old properties
+  Object.keys(prevProps)
+    .filter(isProperty)
+    .filter(isGone(prevProps, nextProps))
+    .forEach(name => {
+      dom[name] = ""
+    })
+  // Set new or changed properties
+  Object.keys(nextProps)
+    .filter(isProperty)
+    .filter(isNew(prevProps, nextProps))
+    .forEach(name => {
+      dom[name] = nextProps[name]
+    })
+  // Add event listeners
+  Object.keys(nextProps)
+    .filter(isEvent)
+    .filter(isNew(prevProps, nextProps))
+    .forEach(name => {
+      const eventType = name.toLowerCase().substring(2)
+      dom.addEventListener(eventType, nextProps[name])
+    })
+}
+
+// 渲染diff 之后的fiber tree 到页面
+function commitRoot () {
   // TODO add nodes to dom
+  deletions.forEach(commitWork)
   commitWork(wipRoot.child)
+  currentRoot = wipRoot
   wipRoot = null
 }
 
-function commitWork(fiber) {
+/**
+ * dfs 递归处理fiber tree
+ * parent => child => sibling
+ */
+function commitWork (fiber) {
   if (!fiber) {
     return
   }
   const domParent = fiber.parent.dom
-  domParent.appendChild(fiber.dom)
+  if (
+    fiber.effectTag === 'PLACEMENT' &&
+    fiber.dom !== null
+  ) {
+    domParent.appendChild(fiber.dom)
+  } else if (
+    fiber.effectTag === 'UPDATE' &&
+    fiber.dom !== null
+  ) {
+    updateDom(
+      fiber.dom,
+      fiber.alternate.props,
+      fiber.props
+    )
+  } else if (fiber.effectTag === 'DELETION') {
+    domParent.removeChild(fiber.dom)
+  }
   commitWork(fiber.child)
   commitWork(fiber.sibling)
 }
 
-function render(element, container) {
+// init
+function render (element, container) {
   // TODO set next unit of work
   wipRoot = {
     dom: container,
     props: {
       children: [element]
-    }
+    },
+    // 保存之前commit 的fiber
+    alternate: currentRoot
   }
+  deletions = []
   nextUnitOfWork = wipRoot
 }
 
 let nextUnitOfWork = null
+let currentRoot = null
 let wipRoot = null
+let deletions = null
 
-function workLoop(deadline) {
+/**
+ * 每一个element 都有一个fiber
+ * 每一个fiber 都是一个work unit
+ * 通过requestIdleCallback 进行模拟scheduler（调度）
+ */
+function workLoop (deadline) {
   let shouldYield = false
   while (nextUnitOfWork && !shouldYield) {
     nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
@@ -86,41 +163,82 @@ function workLoop(deadline) {
 
 requestIdleCallback(workLoop)
 
-function performUnitOfWork(fiber) {
+// 每个work unit 需要处理的三个步骤
+function performUnitOfWork (fiber) {
   // TODO create dom node
   if (!fiber.dom) {
     fiber.dom = createDom(fiber)
   }
   // TODO create new fibers
   const elements = fiber.props.children
-  let index = 0
-  let prevSibling = null
-  while (index < elements.length) {
-    const element = elements[index]
-    const newFiber = {
-      type: element.type,
-      props: element.props,
-      parent: fiber,
-      dom: null
-    }
-    if (index === 0) {
-      fiber.child = newFiber
-    } else {
-      prevSibling.sibling = newFiber
-    }
-    prevSibling = newFiber
-    index++
-  }
+  reconcileChildren(fiber, elements)
+
   // TODO return next unit of work
   if (fiber.child) {
     return fiber.child
   }
   let nextFiber = fiber
-  while(nextFiber) {
+  while (nextFiber) {
     if (nextFiber.sibling) {
       return nextFiber.sibling
     }
     nextFiber = nextFiber.parent
+  }
+}
+
+// 调和 children
+function reconcileChildren (wipFiber, elements) {
+  let index = 0
+  let oldFiber = wipFiber.alternate && wipFiber.alternate.child
+  let prevSibling = null
+  while (
+    index < elements.length ||
+    oldFiber != null
+  ) {
+    const element = elements[index]
+    let newFiber = null
+    // TODO compare oldFiber to element
+    const sameType = oldFiber && element && element.type === oldFiber.type
+
+    if (sameType) {
+      // TODO update the node
+      newFiber = {
+        type: oldFiber.type,
+        props: element.props,
+        dom: oldFiber.dom,
+        parent: wipFiber,
+        alternate: oldFiber,
+        effectTag: 'UPDATE'
+      }
+    }
+    if (element && !sameType) {
+      // TODO add this node
+      newFiber = {
+        type: element.type,
+        props: element.props,
+        dom: null,
+        parent: wipFiber,
+        alternate: null,
+        effectTag: 'PLACEMENT'
+      }
+    }
+    if (oldFiber && !sameType) {
+      // TODO delete the oldFiber's node
+      oldFiber.effectTag = 'DELETION'
+      deletions.push(oldFiber)
+    }
+
+    if (oldFiber) {
+      oldFiber = oldFiber.sibling
+    }
+
+    if (index === 0) {
+      wipFiber.child = newFiber
+    } else {
+      prevSibling.sibling = newFiber
+    }
+    prevSibling = newFiber
+    index++
   }
 }
 
@@ -130,13 +248,20 @@ const LCL = {
 }
 
 /** @jsx LCL.createElement */
-const element = (
-  <div style="background: salmon">
-    <h1>Hello World</h1>
-    <h2 style="text-align:right">from Didact</h2>
-  </div>
-);
+const container = document.getElementById("root")
 
-const container = document.getElementById('root')
+const updateValue = e => {
+  rerender(e.target.value)
+}
 
-LCL.render(element, container)
+const rerender = value => {
+  const element = (
+    <div>
+      <input onInput={updateValue} value={value} />
+      <h2>Hello {value}</h2>
+    </div>
+  )
+  LCL.render(element, container)
+}
+
+rerender("World")
