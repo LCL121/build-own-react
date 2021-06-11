022038cc5f5448092b17f9c5fcb4ee81b4f7b98b
LCL121 <1391436522@qq.com>
2021-06-11 17:26:28 +0800

    step 4 end

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 /** @jsxRuntime classic */
-/** @jsx jsx */
 function createTextElement(text) {
   return {
     type: 'TEXT_ELEMENT',
@@ -24,26 +23,83 @@ function createElement(type, props, ...children) {
   }
 }
 
-function render(element, container) {
-  console.log(element)
-  const dom = element.type === 'TEXT_ELEMENT'
+function createDom(fiber) {
+  const dom = fiber.type === 'TEXT_ELEMENT'
     ? document.createTextNode('')
-    : document.createElement(element.type)
+    : document.createElement(fiber.type)
   
   const isProperty = key => key !== 'children'
-  Object.keys(element.props)
+  Object.keys(fiber.props)
     .filter(isProperty)
     .forEach(name => {
-      dom[name] = element.props[name]
+      dom[name] = fiber.props[name]
     })
 
-  element.props.children.forEach(child => {
-    if (child) {
-      render(child, dom)
-    }
-  })
+  return dom
+}
 
-  container.appendChild(dom)
+function render(element, container) {
+  // TODO set next unit of work
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element]
+    }
+  }
+}
+
+let nextUnitOfWork = null
+
+function workLoop(deadline) {
+  let shouldYield = false
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
+    shouldYield = deadline.timeRemaining() < 1
+  }
+  requestIdleCallback(workLoop)
+}
+
+requestIdleCallback(workLoop)
+
+function performUnitOfWork(fiber) {
+  // TODO add dom node
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber)
+  }
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom)
+  }
+  // TODO create new fibers
+  const elements = fiber.props.children
+  let index = 0
+  let prevSibling = null
+  while (index < elements.length) {
+    const element = elements[index]
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null
+    }
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      prevSibling.sibling = newFiber
+    }
+    prevSibling = newFiber
+    index++
+  }
+  // TODO return next unit of work
+  if (fiber.child) {
+    return fiber.child
+  }
+  let nextFiber = fiber
+  while(nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling
+    }
+    nextFiber = nextFiber.parent
+  }
 }
 
 const LCL = {
